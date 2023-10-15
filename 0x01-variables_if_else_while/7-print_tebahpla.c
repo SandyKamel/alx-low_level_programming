27525a9de3c4bb5e3784376cb04c8dc67c4f3003
SandyKamel <SandyKamel@example.com>
2023-10-15 06:27:22 -0700

    my commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,25 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: print 0-9 using putchar
- *              while using int variable
- *
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
-	int digit = 0;
+	char ch;
 
-	while (digit <= 9)
+	for (ch = 'z'; ch >= 'a'; ch--)
 	{
-		/*convert digit to ASCII representation*/
-		putchar(digit + '0');
-		++digit;
+		putchar(ch);
 	}
 	putchar('\n');
-
 	return (0);
 }
