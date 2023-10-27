a1587694f1e452bc96419614a76bc3f8e639822e
SandyKamel <SandyKamel@example.com>
2023-10-26 17:42:11 -0700

    my commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ *Write a function that prints a string
+ *s = string
+ *Return : void
+ */
+
+void _puts_recursion(char *s);
+{
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
